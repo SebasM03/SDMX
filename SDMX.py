9e54935a5a6cb3c12f62754d73b6f153e3f5579c
Sebas M <sebas.malaver37@gmail.com>
2023-04-25 12:34:16 -0500

    UltimoAjuste

--- SDMX.py
+++ SDMX.py
@@ -11,8 +11,9 @@ import pandas as pd
 
 #Consulta encabezado
 sqlHeader="""
-SELECT TOP 1
+SELECT DISTINCT TOP 2
 	indicator.indicatorID
+	,indicator.indicatorLeyendNameES
 	,serie.freqID
 	,serie.refAreaID
 	,time_format.timeFormatID
@@ -29,8 +30,8 @@ LEFT OUTER JOIN unit ON serie.unitID = unit.unitID
 LEFT OUTER JOIN indicator ON serie.indicatorID =  indicator.indicatorID
 LEFT OUTER JOIN time_format ON serie.timeFormatID = time_format.timeFormatID
 LEFT OUTER JOIN observation_status ON observation_value.obsStatusID = observation_status.obsStatusID
+WHERE indicator.indicatorID = 'NGDP_PA_XDC'
 ;"""
-
 
 #Llamar datos de la base de datos
 sqlheadert=dbu.excecute_sql(sqlHeader)
@@ -48,12 +49,13 @@ df["timeFormatID"] = df["timeFormatID"].str.split(" ", n=1).str[0]
 df["FLARID"] = df["FLARID"].str.split(" ", n=1).str[0]
 
 
+root = ET.Element("MessageGroup")
+root.set("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance")
+root.set("xsi:schemaLocation", "http://www.SDMX.org/resources/SDMXML/schemas/v2_0/message https://flar.com/sie/")
 
-root = ET.Element("MessageGroup", xmlns="https://flar.com/sie/")
 
 #Se crea elemento Header
 header = ET.SubElement(root, "Header")
-
 for x, row in df.iterrows():
     ID = ET.SubElement(header, "ID")
     ID.text = str(row["ID"])
@@ -71,16 +73,15 @@ for x, row in df.iterrows():
     extracted = ET.SubElement(sender, "Extracted")
     extracted.text = str(row["currentDate"])
     
-    dataset = ET.SubElement(root, "DataSet", action="Replace")
-    series = ET.SubElement(dataset, "Series", freqID=str(row["freqID"]), refAreaID=str(row["refAreaID"]), timeFormat=str(row["timeFormatID"]), counterpartArea=str(row["counterpartArea"]), unit=str(row["unitID"]))
 
 
 #Consulta DataSet 
 sql="""
-SELECT TOP 78
+SELECT
 	observation_value.timePeriod
 	,observation_value.obsValue
 	,observation_status.obsStatusName
+	,serie.refAreaID
 FROM observation_value
 LEFT OUTER JOIN serie ON observation_value.serieID =  serie.serieID
 LEFT OUTER JOIN unit_multiplier ON serie.unitMultID = unit_multiplier.unitMultID
@@ -88,19 +89,32 @@ LEFT OUTER JOIN unit ON serie.unitID = unit.unitID
 LEFT OUTER JOIN indicator ON serie.indicatorID =  indicator.indicatorID
 LEFT OUTER JOIN time_format ON serie.timeFormatID = time_format.timeFormatID
 LEFT OUTER JOIN observation_status ON observation_value.obsStatusID = observation_status.obsStatusID
+WHERE indicator.indicatorID = 'NGDP_PA_XDC'
+	AND serie.refAreaID IN ('EC','AR')
+ORDER BY serie.refAreaID, observation_value.timePeriod ASC
 ;"""
+
 
 #Llamar datos de la base de datos
 result=dbu.excecute_sql(sql)
 #Crear DataFrame con Pandas
 ds = pd.DataFrame(result)
+ds["refAreaID"] = ds["refAreaID"].str.split(" ", n=1).str[0]
 
-for i,row in ds.iterrows():
-      values = ET.SubElement(series, "message:Values", timePeriod=str(row["timePeriod"]),obsValue=str(row["obsValue"]),obsStatusName=str(row["obsStatusName"]))
-      
+obs_dict = {}
 
+for x, row in df.iterrows():
+    dataset = ET.SubElement(root, "DataSet", action="Replace")
+    series = ET.SubElement(dataset, "Series", freqID=str(row["freqID"]), refAreaID=str(row["refAreaID"]), timeFormat=str(row["timeFormatID"]), counterpartArea=str(row["counterpartArea"]), unit=str(row["unitID"]))
+    
+   
+    # Filtrar el DataFrame por refAreaID actual
+    filtered_ds = ds[ds["refAreaID"] == row["refAreaID"]]
+
+    # Recorrer el DataFrame filtrado y crear elementos "Obs" dentro de la etiqueta "Series"
+    for i, row in filtered_ds.iterrows():
+        values = ET.SubElement(series, "Obs", timePeriod=str(row["timePeriod"]), obsValue=str(row["obsValue"]), obsStatusName=str(row["obsStatusName"]), refAreaID=str(row["refAreaID"]))
 
 arbol = ET.ElementTree(root)
 arbol.write("SDMX.xml", encoding="utf-8", xml_declaration=True)
-
-
+print(arbol)
