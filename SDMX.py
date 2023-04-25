cbcdad725cce93bad6b86b6112357883f386fba3
Sebas M <sebas.malaver37@gmail.com>
2023-04-25 15:56:08 -0500

    Modificación Series

--- SDMX.py
+++ SDMX.py
@@ -11,18 +11,38 @@ import pandas as pd
 
 #Consulta encabezado
 sqlHeader="""
-SELECT DISTINCT TOP 2
+SELECT 
 	indicator.indicatorID
-	,indicator.indicatorLeyendNameES
+	,indicator.indicatorNameES
+	,indicator.dataDomainID
+	,indicator.categoryID
+	,indicator.indicatorPropriety
+	,indicator.FLARID
+	,indicator.indicatorNameESShort
+	,indicatorLeyendNameES
+	,GETDATE() AS currentDate
+    ,'correo@flar.net'  AS email
+FROM indicator
+WHERE indicator.indicatorID = 'NGDP_PA_XDC'
+;"""
+#Llarmas datos de la base de datos
+sqlheader=dbu.excecute_sql(sqlHeader)
+#Crear DataFrame con Pandas
+dc = pd.DataFrame(sqlheader)
+
+#Consulta DataSet
+sqlDataSet="""
+SELECT DISTINCT 
+    indicator.indicatorID
 	,serie.freqID
 	,serie.refAreaID
 	,time_format.timeFormatID
 	,serie.counterpartArea
 	,unit_multiplier.unitMultID
 	,unit.unitID
-	,indicator.FLARID
-    ,GETDATE() AS currentDate
-    ,'correo@flar.net'  AS email
+    ,indicator.categoryID
+    ,indicator.indicatorPropriety
+    ,indicator.indicatorNameESShort
 FROM observation_value
 LEFT OUTER JOIN serie ON observation_value.serieID =  serie.serieID
 LEFT OUTER JOIN unit_multiplier ON serie.unitMultID = unit_multiplier.unitMultID
@@ -31,22 +51,22 @@ LEFT OUTER JOIN indicator ON serie.indicatorID =  indicator.indicatorID
 LEFT OUTER JOIN time_format ON serie.timeFormatID = time_format.timeFormatID
 LEFT OUTER JOIN observation_status ON observation_value.obsStatusID = observation_status.obsStatusID
 WHERE indicator.indicatorID = 'NGDP_PA_XDC'
+ORDER BY serie.refAreaID ASC
 ;"""
 
-#Llamar datos de la base de datos
-sqlheadert=dbu.excecute_sql(sqlHeader)
-#Crear DataFrame con Pandas
-df = pd.DataFrame(sqlheadert)
+sqldataset=dbu.excecute_sql(sqlDataSet)
+df = pd.DataFrame(sqldataset)
 
-df["indicatorID"] = df["indicatorID"].astype(str)
-df["FLARID"] = df["FLARID"].astype(str)
-df["currentDate"] = df["currentDate"].astype(str)
+
+dc["indicatorID"] = dc["indicatorID"].astype(str)
+dc["FLARID"] = dc["FLARID"].astype(str)
+dc["currentDate"] = dc["currentDate"].astype(str)
 
 #Quitar espacios en los campos
-df["ID"] = df["indicatorID"].str.split(" ", n=1).str[0]
+dc["ID"] = dc["indicatorID"].str.split(" ", n=1).str[0]
+dc["FLARID"] = dc["FLARID"].str.split(" ", n=1).str[0]
 df["refAreaID"] = df["refAreaID"].str.split(" ", n=1).str[0]
 df["timeFormatID"] = df["timeFormatID"].str.split(" ", n=1).str[0]
-df["FLARID"] = df["FLARID"].str.split(" ", n=1).str[0]
 
 
 root = ET.Element("MessageGroup")
@@ -56,9 +76,12 @@ root.set("xsi:schemaLocation", "http://www.SDMX.org/resources/SDMXML/schemas/v2_
 
 #Se crea elemento Header
 header = ET.SubElement(root, "Header")
-for x, row in df.iterrows():
+
+for x, row in dc.iterrows():
     ID = ET.SubElement(header, "ID")
     ID.text = str(row["ID"])
+    Name = ET.SubElement(header, "Name")
+    Name.text = str(row["indicatorLeyendNameES"])
     test = ET.SubElement(header, "Test")
     test.text = "false"
     prepared = ET.SubElement(header, "Prepared")
@@ -68,17 +91,16 @@ for x, row in df.iterrows():
     name.text = "FLAR - Fondo LatinoAmericano de Reservas"
     contact = ET.SubElement(sender, "Contact")
     department = ET.SubElement(contact, "Department", lang="es")
-    department.text = "Gerencia Tecnología y Estudios Económicos - FLAR"
-    uri = ET.SubElement(contact, "URI", email=str(row["email"]))
-    extracted = ET.SubElement(sender, "Extracted")
+    department.text = "FLAR - Gerencia Tecnología y Estudios Económicos"
+    uri = ET.SubElement(contact, "URI",  url="https://flar.com/sie/", email=str(row["email"]))
+    extracted = ET.SubElement(header, "Extracted")
     extracted.text = str(row["currentDate"])
-    
+    dataset = ET.SubElement(root, "DataSet", action="Replace", category=str(row["categoryID"]), propiety=str(row["indicatorPropriety"]), nameshort=str(row["indicatorNameESShort"]))
 
-
-#Consulta DataSet 
+#Consulta Series
 sql="""
 SELECT
-	observation_value.timePeriod
+    observation_value.timePeriod
 	,observation_value.obsValue
 	,observation_status.obsStatusName
 	,serie.refAreaID
@@ -90,7 +112,6 @@ LEFT OUTER JOIN indicator ON serie.indicatorID =  indicator.indicatorID
 LEFT OUTER JOIN time_format ON serie.timeFormatID = time_format.timeFormatID
 LEFT OUTER JOIN observation_status ON observation_value.obsStatusID = observation_status.obsStatusID
 WHERE indicator.indicatorID = 'NGDP_PA_XDC'
-	AND serie.refAreaID IN ('EC','AR')
 ORDER BY serie.refAreaID, observation_value.timePeriod ASC
 ;"""
 
@@ -102,12 +123,9 @@ ds = pd.DataFrame(result)
 ds["refAreaID"] = ds["refAreaID"].str.split(" ", n=1).str[0]
 
 obs_dict = {}
-
 for x, row in df.iterrows():
-    dataset = ET.SubElement(root, "DataSet", action="Replace")
     series = ET.SubElement(dataset, "Series", freqID=str(row["freqID"]), refAreaID=str(row["refAreaID"]), timeFormat=str(row["timeFormatID"]), counterpartArea=str(row["counterpartArea"]), unit=str(row["unitID"]))
     
-   
     # Filtrar el DataFrame por refAreaID actual
     filtered_ds = ds[ds["refAreaID"] == row["refAreaID"]]
 
